745ce525622dcc99efe3e9546ec75005cbb32853
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-04 13:10:22 -0600

    Implement taxable and isVehicle functions
    Added functions to determine tax status and vehicle type.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// Returns "is taxable" only if state is "IA" and taxExempt is false
+function taxable(state, taxExempt) {
+  if (state === "IA" && taxExempt === false) {
+    return "is taxable";
+  }
+}
+
+// Returns "is vehicle" if at least one of the parameters is true
+function isVehicle(hasWheels, canFly, canSwim) {
+  if (hasWheels || canFly || canSwim) {
+    return "is vehicle";
+  }
+}
+
 
